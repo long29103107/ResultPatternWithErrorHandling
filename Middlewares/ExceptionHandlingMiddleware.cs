e57a7fdad150a1799b6cb6a7ce3a61ac1d1ac32c
longnguyen29103107 <long29103107@gmail.com>
2024-09-28 21:18:01 +0700

    add comment

--- Middlewares/ExceptionHandlingMiddleware.cs
+++ Middlewares/ExceptionHandlingMiddleware.cs
@@ -68,6 +68,8 @@ public class ExceptionHandlingMiddleware
                     await _HandleResponseAsync(context, errorMsg);
                 }
             }
+
+//Test cherry pick
         }
     }
 
